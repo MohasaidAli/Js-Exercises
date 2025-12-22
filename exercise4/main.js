ab49190f12854b59c5e7b0990df485a6660646b1
MohasaidAli <mohaseid830@gmail.com>
2025-12-22 06:40:26 +0300

    submited exercise4

---
+++ exercise4/main.js
@@ -0,0 +1,14 @@
+ function add(x,y){
+    return x+y;
+ }
+ let result = add(10,20);
+ add(20,30);
+ add(50,100);
+ add(5,5)
+ console.log(result /2 )
+//    fucntin expression
+ const addNumbers = function add (x,y){
+    return x+y;
+ }
+  let res = add (10,20)
+  console.log(res)
