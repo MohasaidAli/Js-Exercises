3120d541a9d4ae3542be412c9637210a378db368
MohasaidAli <mohaseid830@gmail.com>
2025-12-24 06:27:30 +0300

    submited exercise9 & exercise10

---
+++ exercise9/main.js
@@ -0,0 +1,9 @@
+let car = {
+    made : "china",
+    model : "BMw" ,
+    year : 2020,
+    start : function (){
+        console.log ("the car started")
+    }
+} ;
+car.start();
---
+++ exrcise10/main.js
@@ -0,0 +1,6 @@
+let library = [
+    {title:"java book", author:"yusuf", year:2020},
+    {title:"java book", author:"hasan", year:2021}
+];
+console.log(library[0].title)
+console.log(library[1].author)
