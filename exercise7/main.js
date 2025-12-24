57380d1d0d6fe2e39de7a1fb5e0a31ffefec7937
MohasaidAli <mohaseid830@gmail.com>
2025-12-24 05:47:55 +0300

    exercise6 & exercise7 & exrcise8

---
+++ exercise7/main.js
@@ -0,0 +1,7 @@
+let colors = ["green ", "red" ,"blue"]
+console.log(colors);
+console.log("before push")
+colors.push("yellow")
+console.log(colors)
+console.log("after push")
+colors.unshift("green")
---
+++ exercise8/main.js
@@ -0,0 +1,9 @@
+let car = {
+    made : "china",
+    model : "BMw" ,
+    year : 2020
+}
+console.log(car)
+car.model;
+car.year;
+car.made;
